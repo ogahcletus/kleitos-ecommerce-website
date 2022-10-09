928f180bb94103a35ce2fbea59b7a68b145ca198
Cletus Ogah <ogahcletus@gmail.com>
2022-10-09 18:43:34 +0100

    create react folders

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <a  href="/">kleitos</a>
+      </header>
+      <main>
+        LISTS OF PRODUCTS
+      </main>
+    </div>
+  );
+}
+
+export default App;
