d197f4e528abfc9bf0288dfdd8d51a63d5d70008
Cletus Ogah <ogahcletus@gmail.com>
2022-10-10 09:48:38 +0100

    added codes to implement data retrieve from api using axios

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom';
-import data from '../data';
+//import data from '../data';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 function Homepage() {
+  const[products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/api/products')
+      setProducts(response.data)
+    }
+
+    fetchData();
+
+  }, [])
   return (
     <div>
 <h1>Featured Products</h1>
         <div className='products'>
         {
-          data.products.map((product) => (
+          products.map((product) => (
             <div className='product' key={product.slug}>
             <Link to={`/product/${product.slug}`}>
             <img  src={product.image}  alt={product.name} className='product-image'/>
